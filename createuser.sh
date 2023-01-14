cbd12b09d4710aa78c13a207fe0cda8bf9a69694
mahmoud.abdelfatah-at-074697765782 <mahmoud.abdelfatah707@gmail.com>
2023-01-14 03:04:11 +0200

    updating scripts

--- createuser.sh
+++ createuser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-useradd -p "WB-Services2022" -G sudo -m -d /home/WB-Services -s /bin/bash WB-Services
+
+read -p "Enter password for the new user: " PASSWORD
+
+useradd -p $PASSWORD -G sudo -m -d /home/WB-Services -s /bin/bash WB-Services
 #execute sudo commands without password
 echo "WB-Services ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
